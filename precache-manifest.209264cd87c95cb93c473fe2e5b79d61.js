7609e7f461b13280343209fe792b114e41c8f159
ompuno <ompuno@yahoo.fr>
2019-01-28 15:12:58 +0100

    Ajout des fichiers initiaux

---
+++ precache-manifest.209264cd87c95cb93c473fe2e5b79d61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9cc8154eecfb79bfd5b2",
+    "url": "/js/chunk-vendors.72dd1bbd.js"
+  },
+  {
+    "revision": "b038a157e953e3da0cd4",
+    "url": "/js/app.29626a31.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "/js/about.0d02e657.js"
+  },
+  {
+    "revision": "152ffb1cf824cc62a26cbca595b4b78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b038a157e953e3da0cd4",
+    "url": "/css/app.84fe17e1.css"
+  }
+];
